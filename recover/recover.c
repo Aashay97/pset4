a6481e1deb35cda8c030d8248314061f861af084
Aashay Mehta <aashaym97@gmail.com>
2017-04-19 15:38:09 +0000

    recover done

--- recover/recover.c
+++ recover/recover.c
@@ -5,6 +5,7 @@ from a memory card*/
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdint.h>
 
 typedef uint8_t BYTE;
 
@@ -17,6 +18,7 @@ int main(int argc,char *argv[])
         return 1;
     }
     
+    //open input file
     FILE *inptr = fopen(argv[1],"r");
     if(inptr==NULL)
     {
@@ -24,29 +26,44 @@ int main(int argc,char *argv[])
         return 2;
     }
     
-    int count=0,flag=0;
+    int count=0;
     
+    //create a block of 512 bytes
     BYTE buffer[512];
     
     char name[8];
     
-    FILE *outptr;
+    
+    FILE *outptr=NULL;
     
     while(fread(buffer,512,1,inptr))
     {
+        //check for start of new jpeg
         if(buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3] & 0xf0)==0xe0)
         {
+            //close previous file
             if(outptr!=NULL)
             {
                 fclose(outptr);
             }
             
-            sprintf(name,"%03i",count);
+            //create new file
+            sprintf(name,"%03i.jpg",count);
             count++;
             
+            //open output file
             outptr=fopen(name,"w");
             
             fwrite(buffer,sizeof(buffer),1,outptr);
         }
+        
+        else if(outptr!=NULL)
+        {
+            fwrite(buffer,sizeof(buffer),1,outptr);
+        }
     }
+    
+    fclose(outptr);
+    fclose(inptr);
+    return 0;
 }
